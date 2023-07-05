59a0f776c17156f85a0171a3d1a85f69336fbbbe
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-07-05 17:59:22 +0300

    feat: добавлены первые версии Dto для моделей Item и User.

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.item.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Builder
+@Jacksonized
+@Data
+@AllArgsConstructor
 public class ItemDto {
+    private Long id;
+    private String name;
+    private String description;
+    @JsonProperty("available")
+    private boolean isAvailable;
 }
