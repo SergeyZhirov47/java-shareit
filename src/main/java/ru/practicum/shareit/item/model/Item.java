cf9017854ff3171cb7885790334343aedfe49f82
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-07-05 17:43:33 +0300

    feat: добавлены первые версии моделей Item и User.

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@AllArgsConstructor
 public class Item {
+    private Long id; // уникальный идентификатор вещи
+    private String name; // краткое название
+    private String description; // развёрнутое описание
+    private boolean isAvailable; // статус о том, доступна или нет вещь для аренды
+    private User owner; // владелец вещи
+    // если вещь была создана по запросу другого пользователя,
+    // то в этом поле будет храниться ссылка на соответствующий запрос
+    private ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@AllArgsConstructor
 public class User {
+    private Long id;  // уникальный идентификатор пользователя
+    private String name; // имя или логин пользователя
+    @Email
+    private String email; // адрес электронной почты. Уникален для каждого пользователя.
 }
