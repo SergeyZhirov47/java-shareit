566726ddcb6f5b0e2b9c9183fb90e1dacb7e7a75
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-07-06 14:29:04 +0300

    feat: изменение модели User (поля должны быть заполнены).

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,18 +1,23 @@
-package ru.practicum.shareit.user;
+package ru.practicum.shareit.user.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 /**
  * TODO Sprint add-controllers.
  */
+@Builder
 @Data
 @AllArgsConstructor
 public class User {
     private Long id;  // уникальный идентификатор пользователя
+    @NotBlank
     private String name; // имя или логин пользователя
+    @NotBlank
     @Email
     private String email; // адрес электронной почты. Уникален для каждого пользователя.
 }
