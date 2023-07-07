2ee9964aa3b6e09e9f89d785037589c905de15c9
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-07-07 22:59:03 +0300

    code: привел логи к формату как в контроллере для пользователя.

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -16,35 +16,50 @@ import java.util.List;
 @AllArgsConstructor
 @Slf4j
 public class ItemController {
-    private final static String USER_ID_REQUEST_HEADER = "X-Sharer-User-Id";
+    private static final String USER_ID_REQUEST_HEADER = "X-Sharer-User-Id";
     private final ItemService itemService;
 
+    // Добавление вещи
     @PostMapping
     public ItemDto add(@RequestHeader(USER_ID_REQUEST_HEADER) long ownerId, @Valid @RequestBody ItemCreateDto item) {
         log.info(String.format("POST /items, body = %s, %s = %s", item, USER_ID_REQUEST_HEADER, ownerId));
-        return itemService.createAndGet(item, ownerId);
+        final ItemDto newItem = itemService.createAndGet(item, ownerId);
+        log.info(String.format("Успешно добавлена вещь с id = %s", newItem.getId()));
+
+        return newItem;
     }
 
+    // Обновление информации о вещи ее владельцем
     @PatchMapping("/{itemId}")
     public ItemDto update(@RequestHeader(USER_ID_REQUEST_HEADER) long userId,
                           @Valid @RequestBody ItemDto item,
                           @PathVariable(name = "itemId") long itemId) {
         log.info(String.format("PATCH /items/{itemId}, body = %s, {itemId} = %s, %s = %s", item, itemId, USER_ID_REQUEST_HEADER, userId));
-        return itemService.update(itemId, item, userId);
+        final ItemDto updatedItem = itemService.update(itemId, item, userId);
+        log.info(String.format("Успешно обновлены данные вещи с id = %s", updatedItem.getId()));
+
+        return updatedItem;
     }
 
+    // Получение информации о вещи пользователем
     @GetMapping("/{itemId}")
     public ItemDto getItemById(@RequestHeader(USER_ID_REQUEST_HEADER) long userId,
-                          @PathVariable(name = "itemId") long itemId) {
+                               @PathVariable(name = "itemId") long itemId) {
         log.info(String.format("GET /items/{itemId}, {itemId} = %s, %s = %s", itemId, USER_ID_REQUEST_HEADER, userId));
-        return itemService.getById(itemId);
+        final ItemDto item = itemService.getById(itemId);
+        log.info(String.format("Успешно получены данные о вещи с id = %s", item.getId()));
+
+        return item;
     }
 
     // Просмотр владельцем списка всех его вещей с указанием названия и описания для каждой
     @GetMapping
     public List<ItemDto> getAllOwnerItems(@RequestHeader(USER_ID_REQUEST_HEADER) long ownerId) {
         log.info(String.format("GET /items, %s = %s", USER_ID_REQUEST_HEADER, ownerId));
-        return itemService.getAllOwnerItems(ownerId);
+        final List<ItemDto> ownerItems = itemService.getAllOwnerItems(ownerId);
+        log.info(String.format("Успешно получены вещи (%s штук) пользователя с id = %s", ownerItems.size(), ownerId));
+
+        return ownerItems;
     }
 
     // Поиск вещи потенциальным арендатором.
@@ -53,6 +68,9 @@ public class ItemController {
     public List<ItemDto> searchItems(@RequestHeader(USER_ID_REQUEST_HEADER) long userId,
                                      @NotBlank @RequestParam(name = "text") String text) {
         log.info(String.format("GET /items/search?text=text, text = %s, %s = %s", text, USER_ID_REQUEST_HEADER, userId));
-        return itemService.searchItems(text, userId);
+        final List<ItemDto> searchedItems = itemService.searchItems(text, userId);
+        log.info(String.format("Успешно получены вещи (%s штук) по запросу \"%s\" пользователя с id = %s", searchedItems.size(), text, userId));
+
+        return searchedItems;
     }
 }
