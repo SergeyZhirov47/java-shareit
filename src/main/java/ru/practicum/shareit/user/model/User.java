eff0075c05028641239c108e18de0ba10e2917a4
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-07-13 16:04:25 +0300

    feat: добавлены аннотации для работы с моделью через JPA

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+@Entity
+@Table(name = "users")
 @Builder
 @Data
 @AllArgsConstructor
 public class User {
+    @Id
     private Long id;  // уникальный идентификатор пользователя
     @NotBlank
     private String name; // имя или логин пользователя
