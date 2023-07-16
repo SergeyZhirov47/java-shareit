0de724edcdd5ebfaee1f51bb0dc6dff66b8ff8e7
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-07-16 20:48:08 +0300

    feat: добавлены затовки контроллера, репозитория и сервиса для работы с Booking.

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,74 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingCreateDto;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.model.BookingStateForSearch;
+import ru.practicum.shareit.booking.model.BookingStatus;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /**
  * TODO Sprint add-bookings.
  */
 @RestController
 @RequestMapping(path = "/bookings")
+@Validated
+@AllArgsConstructor
+@Slf4j
 public class BookingController {
+    // ToDo!
+    // Такой же используется и в ItemController
+    // вынести в отдельное место
+    private static final String USER_ID_REQUEST_HEADER = "X-Sharer-User-Id";
+
+    //    Добавление нового запроса на бронирование.
+    //    Запрос может быть создан любым пользователем, а затем подтверждён владельцем вещи.
+    //    Эндпоинт — POST /bookings. После создания запрос находится в статусе WAITING — «ожидает подтверждения».
+    // @Valid
+    @PostMapping
+    public BookingDto create(@RequestHeader(USER_ID_REQUEST_HEADER) long userId, @RequestBody BookingCreateDto newBooking) {
+        return null;
+    }
+
+    //    Подтверждение или отклонение запроса на бронирование. Может быть выполнено только владельцем вещи.
+    //    Затем статус бронирования становится либо APPROVED, либо REJECTED.
+    //    Эндпоинт — PATCH /bookings/{bookingId}?approved={approved}, параметр approved может принимать значения true или false.
+    @PatchMapping("/{bookingId}")
+    public BookingDto approveBooking(@RequestHeader(USER_ID_REQUEST_HEADER) long ownerId,
+                                     @RequestPart(name = "bookingId") long bookingId,
+                                     @RequestParam(name = "approved") boolean approved) {
+        return null;
+    }
+
+    //    Получение данных о конкретном бронировании (включая его статус). Может быть выполнено либо автором бронирования, либо владельцем вещи, к которой относится бронирование.
+    //    Эндпоинт — GET /bookings/{bookingId}.
+    @GetMapping("/{bookingId}")
+    public BookingDto getBooking(@RequestHeader(USER_ID_REQUEST_HEADER) long userId, @RequestPart(name = "bookingId") long bookingId) {
+        return null;
+    }
+
+    // ToDo
+    // тут state (статус) интересный.
+    // Некоторые состояния от BookingStatus, а другие свои.
+
+    //    Получение списка всех бронирований текущего пользователя.
+    //    Эндпоинт — GET /bookings?state={state}. Параметр state необязательный и по умолчанию равен ALL (англ. «все»).
+    //    Также он может принимать значения CURRENT (англ. «текущие»), **PAST** (англ. «завершённые»), FUTURE (англ. «будущие»), WAITING (англ. «ожидающие подтверждения»), REJECTED (англ. «отклонённые»). Б
+    //    ронирования должны возвращаться отсортированными по дате от более новых к более старым.
+    @GetMapping
+    public List<BookingDto> getBookingByState(@RequestParam(name = "state", defaultValue = "ALL") BookingStateForSearch state) {
+        return null;
+    }
+
+    //    Получение списка бронирований для всех вещей текущего пользователя.
+    //    Эндпоинт — GET /bookings/owner?state={state}. Этот запрос имеет смысл для владельца хотя бы одной вещи. Работа параметра state аналогична его работе в предыдущем сценарии.
+    @GetMapping("/owner")
+    public List<BookingDto> getOwnerBookingsByState(@RequestHeader(USER_ID_REQUEST_HEADER) long ownerId, @RequestParam(name = "state", defaultValue = "ALL") BookingStateForSearch state) {
+        return null;
+    }
 }
---
+++ src/main/java/ru/practicum/shareit/booking/model/BookingStateForSearch.java
@@ -0,0 +1,12 @@
+package ru.practicum.shareit.booking.model;
+
+// ToDo
+// Некоторые состояния от BookingStatus, а другие свои.
+public enum BookingStateForSearch {
+    ALL,
+    CURRENT,
+    PAST,
+    FUTURE,
+    WAITING,
+    REJECTED
+}
