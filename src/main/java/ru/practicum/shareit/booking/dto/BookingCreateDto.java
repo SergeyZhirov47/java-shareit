01203bfb1d6d2110ab2bb6a7245da862aa1ef4f1
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-07-16 20:45:43 +0300

    feat: добавлена таблица bookings и модель теперь связана с БД.

--- src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
@@ -5,25 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
-import ru.practicum.shareit.booking.model.BookingStatus;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
-/**
- * TODO Sprint add-bookings.
- */
 @Builder
 @Jacksonized
 @Data
 @RequiredArgsConstructor
 @AllArgsConstructor
-public class BookingDto {
-    private Long id;
-    private Item item;
-    private User booker;
+public class BookingCreateDto {
+    private long itemId;
     private LocalDateTime start;
     private LocalDateTime end;
-    private BookingStatus status;
 }
--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,22 +1,43 @@
 package ru.practicum.shareit.booking.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 /**
  * TODO Sprint add-bookings.
  */
+@Entity
+@Table(name = "bookings")
+@Builder
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // уникальный идентификатор бронирования
-    private LocalDateTime start; // дата и время начала бронирования
-    private LocalDateTime end; // дата и время конца бронирования
+    @ManyToOne
+    @JoinColumn(name = "item_id")
     private Item item; // вещь, которую пользователь бронирует
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User booker; // пользователь, который осуществляет бронирование
+    @Column(name = "start_booking")
+    private LocalDateTime start; // дата и время начала бронирования
+    @Column(name = "end_booking")
+    private LocalDateTime end; // дата и время конца бронирования
+    // ToDo
+    // хотелось бы хранить не строку, id статуса.
+    // но хранение по порядку тоже не очень.
+    // https://www.baeldung.com/jpa-persisting-enums-in-jpa - посмотреть раздел 4.
+    @Enumerated(EnumType.STRING)
     private BookingStatus status; // статус бронирования
 }
