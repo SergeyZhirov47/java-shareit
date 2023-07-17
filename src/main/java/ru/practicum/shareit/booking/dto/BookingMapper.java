fcd495fdc0f048e96f977b9d9463cd71e6b5f414
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-07-17 19:52:27 +0300

    feat: Добавлен маппер для Booking

--- src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
@@ -3,57 +3,35 @@ package ru.practicum.shareit.booking.dto;
 import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.common.AbstractMapper;
-import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.dto.ItemMapper;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.item.repository.ItemRepository;
-import ru.practicum.shareit.item.service.ItemService;
-
-import java.util.Optional;
-
 
 @UtilityClass
 public class BookingMapper extends AbstractMapper {
-    private ItemService itemService;
-    private ItemRepository itemRepository;
-
     public BookingDto toBookingDto(Booking booking) {
         return BookingDto.builder()
                 .id(booking.getId())
                 .start(booking.getStart())
                 .end(booking.getEnd())
                 .item(booking.getItem())
+                .booker(booking.getBooker())
+                .status(booking.getStatus())
                 .build();
     }
 
     public Booking toBooking(BookingCreateDto bookingCreateDto) {
-        // ToDo
-        // как передать ссылку на предмет, если известен только id
-        // ToDo
-        // Сервис возвращает только DTO. не удобно.
-        // два метода нужно? Или контроллер пускай сам конвертирует в dto?
-        // или использовать репозиторий
-       //final ItemDto itemDto = itemService.getById(bookingCreateDto.getItemId());
-       // ItemMapper.toItem() тут тогда еще и пользователь нужен
-
-        // если через репозиторий, то опять проверки. Не понимаю как со всем этим правильно работать.
-        final Optional<Item> itemOpt = itemRepository.findById(bookingCreateDto.getItemId());
-
-        if (itemOpt.isEmpty()) {
-            return null;
-        }
-        final Item item = itemOpt.get();
-
         return Booking.builder()
                 .start(bookingCreateDto.getStart())
                 .end(bookingCreateDto.getEnd())
-                .item(item)
                 .build();
     }
 
     public Booking updateIfDifferent(final Booking booking, final BookingDto bookingWithChanges) {
         return Booking.builder()
-
+                .id(booking.getId())
+                .item(booking.getItem())
+                .booker(booking.getBooker())
+                .start(getChanged(booking.getStart(), bookingWithChanges.getStart()))
+                .end(getChanged(booking.getEnd(), bookingWithChanges.getEnd()))
+                .status(getChanged(booking.getStatus(), bookingWithChanges.getStatus()))
                 .build();
     }
 }
