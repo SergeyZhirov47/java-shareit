e4530c126f57325e04ede805524b8f89746cf14b
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-07-19 11:02:51 +0300

    refactor: тесты требует, чтобы поле с текстом ошибки называлось error

--- src/main/java/ru/practicum/shareit/common/ErrorResponseData.java
+++ src/main/java/ru/practicum/shareit/common/ErrorResponseData.java
@@ -1,10 +1,16 @@
 package ru.practicum.shareit.common;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
+@Builder
+@Jacksonized
 @AllArgsConstructor
 @Data
 public class ErrorResponseData {
+    @JsonProperty("error")
     private String message;
 }
