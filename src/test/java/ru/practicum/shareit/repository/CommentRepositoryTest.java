8abe1fea7e4718f89aca2d561217a2badf90026c
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-08-07 18:49:23 +0300

    feat: добавлен класс для тестов репозитория комментариев

---
+++ src/test/java/ru/practicum/shareit/repository/CommentRepositoryTest.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.item.repository.CommentRepository;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class CommentRepositoryTest {
+    private final CommentRepository commentRepository;
+    private TestEntityManager em;
+
+    @Test
+    public void test() {
+
+    }
+}
