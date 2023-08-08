a8dc3017f9211aa1f488f7c4faf23c2e6b8bb17a
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-08-08 21:30:11 +0300

    feat: завершены тесты для контроллера пользователей

--- src/test/java/ru/practicum/shareit/controller/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/controller/UserControllerTest.java
@@ -13,14 +13,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.user.UserController;
 import ru.practicum.shareit.user.dto.UserCreateDto;
 import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.exception.EmailAlreadyUsedException;
+import ru.practicum.shareit.user.exception.UserNotFoundException;
 import ru.practicum.shareit.user.service.UserService;
 
 import java.nio.charset.StandardCharsets;
 
-import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,8 +41,9 @@ public class UserControllerTest {
             .email("johnDoe@somemail.com")
             .build();
 
+    @SneakyThrows
     @Test
-    public void create() throws Exception {
+    public void create() {
         final UserCreateDto correctUser = UserCreateDto.builder()
                 .name("John Doe")
                 .email("johnDoe@somemail.com")
@@ -54,15 +57,16 @@ public class UserControllerTest {
                         .characterEncoding(StandardCharsets.UTF_8)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(userDto.getId())))
-                .andExpect(jsonPath("$.name", is(userDto.getName())))
-                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+                .andExpect(jsonPath("$.id").value(userDto.getId()))
+                .andExpect(jsonPath("$.name").value(userDto.getName()))
+                .andExpect(jsonPath("$.email").value(userDto.getEmail()));
 
         verify(userService).createAndGet(correctUser);
     }
 
+    @SneakyThrows
     @Test
-    public void create_thenNoEmail_thenReturnBadRequest() throws Exception {
+    public void create_thenNoEmail_thenReturnBadRequest() {
         final UserCreateDto userWithoutEmail = UserCreateDto.builder()
                 .name("John Doe")
                 .build();
@@ -73,10 +77,13 @@ public class UserControllerTest {
                         .characterEncoding(StandardCharsets.UTF_8)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+
+        verify(userService, never()).createAndGet(userWithoutEmail);
     }
 
+    @SneakyThrows
     @Test
-    public void create_thenInvalidEmail_thenReturnBadRequest() throws Exception {
+    public void create_thenInvalidEmail_thenReturnBadRequest() {
         final UserCreateDto userWithInvalidEmail = UserCreateDto.builder()
                 .name("John Doe")
                 .email("not valid email")
@@ -88,37 +95,157 @@ public class UserControllerTest {
                         .characterEncoding(StandardCharsets.UTF_8)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
-    }
 
-    @Test
-    public void update() {
-
+        verify(userService, never()).createAndGet(userWithInvalidEmail);
     }
 
     @SneakyThrows
     @Test
-    public void getById() {
-        var userId = 1L;
-        final UserDto userDto = UserDto.builder()
+    public void update_whenUpdateWhole_thenReturnUser() {
+        final long userId = 1L;
+        final UserDto updatedUser = UserDto.builder()
+                .name("new John Doe")
+                .email("newJohnDoe@somemail.com")
+                .build();
+        final UserDto returnedUser = UserDto.builder()
                 .id(userId)
+                .name(updatedUser.getName())
+                .email(updatedUser.getEmail())
                 .build();
 
+        Mockito.when(userService.update(userId, updatedUser)).thenReturn(returnedUser);
+
+        mockMvc.perform(patch(BASE_ENDPOINT + "/{id}", userId)
+                        .content(objectMapper.writeValueAsBytes(updatedUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(returnedUser.getId()))
+                .andExpect(jsonPath("$.name").value(returnedUser.getName()))
+                .andExpect(jsonPath("$.email").value(returnedUser.getEmail()));
+    }
+
+    @SneakyThrows
+    @Test
+    public void update_whenNameOnly_thenReturnUser() {
+        final long userId = 1L;
+        final UserDto updatedUser = UserDto.builder()
+                .name("new John Doe")
+                .build();
+        final UserDto returnedUser = UserDto.builder()
+                .id(userId)
+                .name(updatedUser.getName())
+                .email("JohnDoe@somemail.com")
+                .build();
+
+        Mockito.when(userService.update(userId, updatedUser)).thenReturn(returnedUser);
+
+        mockMvc.perform(patch(BASE_ENDPOINT + "/{id}", userId)
+                        .content(objectMapper.writeValueAsBytes(updatedUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(returnedUser.getId()))
+                .andExpect(jsonPath("$.name").value(returnedUser.getName()))
+                .andExpect(jsonPath("$.email").value(returnedUser.getEmail()));
+    }
+
+    @SneakyThrows
+    @Test
+    public void update_whenEmailOnly_thenReturnUser() {
+        final long userId = 1L;
+        final UserDto updatedUser = UserDto.builder()
+                .email("newJohnDoe@somemail.com")
+                .build();
+        final UserDto returnedUser = UserDto.builder()
+                .id(userId)
+                .name("John Doe")
+                .email(updatedUser.getEmail())
+                .build();
+
+        Mockito.when(userService.update(userId, updatedUser)).thenReturn(returnedUser);
+
+        mockMvc.perform(patch(BASE_ENDPOINT + "/{id}", userId)
+                        .content(objectMapper.writeValueAsBytes(updatedUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(returnedUser.getId()))
+                .andExpect(jsonPath("$.name").value(returnedUser.getName()))
+                .andExpect(jsonPath("$.email").value(returnedUser.getEmail()));
+    }
+
+    @SneakyThrows
+    @Test
+    public void update_whenEmailAlreadyExists_thenReturnConflict() {
+        Mockito.when(userService.update(userDto.getId(), userDto)).thenThrow(EmailAlreadyUsedException.class);
+
+        mockMvc.perform(patch(BASE_ENDPOINT + "/{id}", userDto.getId())
+                        .content(objectMapper.writeValueAsBytes(userDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict());
+    }
+
+    @SneakyThrows
+    @Test
+    public void getById_whenUserExists_thenReturnUser() {
+        final long userId = userDto.getId();
         Mockito.when(userService.getById(userId)).thenReturn(userDto);
 
-        mockMvc.perform(get(BASE_ENDPOINT + "/{id}", userId))
+        mockMvc.perform(get(BASE_ENDPOINT + "/{id}", userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userDto.getId()))
+                .andExpect(jsonPath("$.name").value(userDto.getName()))
+                .andExpect(jsonPath("$.email").value(userDto.getEmail()));
 
         verify(userService).getById(userId);
     }
 
+    @SneakyThrows
     @Test
-    public void getAll() {
+    public void getById_whenUserNotExists_thenReturnNotFound() {
+        final long userNotExistedId = 9999L;
+        Mockito.when(userService.getById(anyLong())).thenThrow(UserNotFoundException.class);
 
+        mockMvc.perform(get(BASE_ENDPOINT + "/{id}", userNotExistedId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 
+    @SneakyThrows
+    @Test
+    public void getAll() {
+        mockMvc.perform(get(BASE_ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(userService).getAll();
+    }
+
+    @SneakyThrows
     @Test
     public void deleteById() {
+        final long userID = 1L;
 
+        mockMvc.perform(delete(BASE_ENDPOINT + "/{id}", userID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(userService).delete(userID);
     }
 }
