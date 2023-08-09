952dba09b2c071eef1a029193dcc82ea383df783
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-08-09 23:28:52 +0300

    feat: большая часть тестов для ItemRequestController

--- src/test/java/ru/practicum/shareit/controller/ItemRequestControllerTest.java
+++ src/test/java/ru/practicum/shareit/controller/ItemRequestControllerTest.java
@@ -1,21 +1,207 @@
 package ru.practicum.shareit.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.request.ItemRequestController;
+import ru.practicum.shareit.request.dto.ItemRequestCreateDto;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.exception.ItemRequestNotFoundException;
 import ru.practicum.shareit.request.service.ItemRequestService;
+import ru.practicum.shareit.user.exception.UserNotFoundException;
+import ru.practicum.shareit.user.repository.DaoUser;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.practicum.shareit.common.ConstantParamStorage.USER_ID_REQUEST_HEADER;
 
 @WebMvcTest(controllers = ItemRequestController.class)
-//@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ItemRequestControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-
+    private final static String BASE_ENDPOINT = "/requests";
+    private final MockMvc mockMvc;
+    private final ObjectMapper objectMapper;
     @MockBean
-    private ItemRequestService itemRequestService;
+    private final DaoUser daoUser;
+    @MockBean
+    private final ItemRequestService itemRequestService;
+
+    private final long userId = 1;
+    private final long requestId = 1;
+    private final ItemRequestDto defaultEmptyItemRequestDto = ItemRequestDto.builder().build();
+
+    @SneakyThrows
+    @Test
+    public void addItemRequest_whenOk_thenReturnOk() {
+        final ItemRequestCreateDto itemRequestCreateDto = ItemRequestCreateDto.builder()
+                .description("need mock")
+                .build();
+
+        Mockito.when(itemRequestService.createAndGet(itemRequestCreateDto, userId)).thenReturn(defaultEmptyItemRequestDto);
+
+        mockMvc.perform(post(BASE_ENDPOINT)
+                        .content(objectMapper.writeValueAsBytes(itemRequestCreateDto))
+                        .header(USER_ID_REQUEST_HEADER, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(itemRequestService).createAndGet(itemRequestCreateDto, userId);
+    }
+
+    @SneakyThrows
+    @Test
+    public void addItemRequest_whenUserNotExists_thenReturn404() {
+        final ItemRequestCreateDto itemRequestCreateDto = ItemRequestCreateDto.builder()
+                .description("need mock")
+                .build();
+
+        Mockito.when(itemRequestService.createAndGet(itemRequestCreateDto, userId))
+                .thenThrow(UserNotFoundException.class);
+
+        mockMvc.perform(post(BASE_ENDPOINT)
+                        .content(objectMapper.writeValueAsBytes(itemRequestCreateDto))
+                        .header(USER_ID_REQUEST_HEADER, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+
+        verify(itemRequestService).createAndGet(itemRequestCreateDto, userId);
+    }
+
+    @SneakyThrows
+    @Test
+    public void addItemRequest_whenDescriptionIsNull_thenReturn400() {
+        final ItemRequestCreateDto requestWithNullDescription = ItemRequestCreateDto.builder()
+                .description(null)
+                .build();
+
+        mockMvc.perform(post(BASE_ENDPOINT)
+                        .content(objectMapper.writeValueAsBytes(requestWithNullDescription))
+                        .header(USER_ID_REQUEST_HEADER, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        verify(itemRequestService, never()).createAndGet(requestWithNullDescription, userId);
+    }
+
+    @SneakyThrows
+    @Test
+    public void addItemRequest_whenDescriptionIsBlank_thenReturn400() {
+        final ItemRequestCreateDto requestWithNullDescription = ItemRequestCreateDto.builder()
+                .description("")
+                .build();
+
+        mockMvc.perform(post(BASE_ENDPOINT)
+                        .content(objectMapper.writeValueAsBytes(requestWithNullDescription))
+                        .header(USER_ID_REQUEST_HEADER, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        verify(itemRequestService, never()).createAndGet(requestWithNullDescription, userId);
+    }
+
+    @SneakyThrows
+    @Test
+    public void getAllUserItemRequests_whenOk_thenReturnOk() {
+        Mockito.when(itemRequestService.getAllUserItemRequests(userId))
+                .thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get(BASE_ENDPOINT)
+                        .header(USER_ID_REQUEST_HEADER, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(itemRequestService).getAllUserItemRequests(userId);
+    }
+
+    @SneakyThrows
+    @Test
+    public void getAllUserItemRequests_whenUserNotExists_thenReturn404() {
+        Mockito.when(itemRequestService.getAllUserItemRequests(userId))
+                .thenThrow(UserNotFoundException.class);
+
+        mockMvc.perform(get(BASE_ENDPOINT)
+                        .header(USER_ID_REQUEST_HEADER, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+
+        verify(itemRequestService).getAllUserItemRequests(userId);
+    }
+
+    // ToDo
+    // тут еще пагинацию проверять
+    @Test
+    public void getAllItemRequests_when_then() {
+
+    }
+
+    @SneakyThrows
+    @Test
+    public void getItemRequest_whenOk_thenReturnOk() {
+        Mockito.when(itemRequestService.getItemRequestById(requestId))
+                .thenReturn(defaultEmptyItemRequestDto);
+
+        mockMvc.perform(get(BASE_ENDPOINT + "/{requestId}", requestId)
+                        .header(USER_ID_REQUEST_HEADER, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(itemRequestService).getItemRequestById(requestId);
+    }
+
+    @SneakyThrows
+    @Test
+    public void getItemRequest_whenRequestNotExists_thenReturn404() {
+        Mockito.when(itemRequestService.getItemRequestById(requestId))
+                        .thenThrow(ItemRequestNotFoundException.class);
+
+        mockMvc.perform(get(BASE_ENDPOINT + "/{requestId}", requestId)
+                        .header(USER_ID_REQUEST_HEADER, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+
+        verify(itemRequestService).getItemRequestById(requestId);
+    }
+
+    @SneakyThrows
+    @Test
+    public void getItemRequest_whenUserNotExists_thenReturn404() {
+        doThrow(UserNotFoundException.class).when(daoUser).checkUserExists(userId);
+
+        mockMvc.perform(get(BASE_ENDPOINT + "/{requestId}", requestId)
+                        .header(USER_ID_REQUEST_HEADER, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+
+        verify(itemRequestService, never()).getItemRequestById(requestId);
+    }
 }
